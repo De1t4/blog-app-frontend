9316c351c40c5aa61d8a0cdfacae3b595c9d4fc7
De1t4 <mauriciochambicaceres@gmail.com>
2024-04-24 18:52:05 -0300

    fixed link

--- app/Favorites/[id]/page.tsx
+++ app/Favorites/[id]/page.tsx
@@ -30,7 +30,7 @@ export default function Page() {
     <section className="flex items-center flex-col max-md:mx-2 ">
       <h1 className=' text-3xl font-semibold text-[#328FFF]'>Tus Favoritos</h1>
       {favoriteData.length !== 0 ? (favoriteData.map((favorite, index)=>(
-        <Link key={favorite.idPosts} href={`/singlePost/${favorite.idPosts}`} className='transition-all duration-300  w-full'>
+        <Link key={favorite.idPosts} href={`/SinglePost/${favorite.idPosts}`} className='transition-all duration-300  w-full'>
           <article key={index} className=" m-auto max-md:w-11/12 border-[1px] hover:scale-105  border-slate-600 shadow-md shadow-slate-800 hover:shadow-slate-700 hover:border-slate-300 w-[38rem]  transition-all duration-300 py-4 px-6 bg-slate-800 h-min-72 my-4 flex flex-col justify-between rounded-lg">
             <h1 className="hover:brightness-110 transition-all duration-300 text-[#328FFF] font-bold text-2xl font-sans max-md:text-xl">{favorite.title}</h1>
             <p className=' text-zinc-100'>{favorite.content}</p>
